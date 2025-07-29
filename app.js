ce785a6a903947f95f1c97c5c35a5627762438a7
Geir Hilmersen <geir.hilmersen@protonmail.com>
2025-07-29 20:16:37 +0200

    first commit! adding some basic project files and folders

---
+++ app.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const express= require('express');
+const app = express();
+
+const router = require('./routers/default_router');
+
+app.use(express.static('public'));
+app.use(router);
+
+const PORT = process.env.PORT;
+app.listen(PORT, ()=> {
+    console.info(`${process.env.APPNAME} is running!`);
+})
