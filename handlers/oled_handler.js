05a971a740330ab4f12d469c2492f9f5057dfefa
Geir Hilmersen <geir.hilmersen@protonmail.com>
2025-08-07 17:54:49 +0200

    adding handlers/oled_handler.js and oled_controller.js with some crude functionality

---
+++ handlers/oled_handler.js
@@ -0,0 +1,18 @@
+const i2c = require('i2c-bus');
+const Oled = require('oled-i2c-bus');
+const font = require('oled-font-5x7');
+
+const i2cBus = i2c.openSync(1);
+const oled = new Oled(i2cBus, {
+    widt: 128,
+    height: 64,
+    address: 0x3c
+});
+
+function showMessage(text) {
+    oled.clearDisplay();
+    oled.setCursor(0,0);
+    oled.writeString(font,1,text,1,true);
+}
+
+showMessage('SPACEPI!');
