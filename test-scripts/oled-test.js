8e71a1e7db2ad77bc62a2cb558be23f6e1416a41
Geir Hilmersen <geir.hilmersen@protonmail.com>
2025-08-07 18:49:04 +0200

    adding test-scripts/oled-test.js

---
+++ test-scripts/oled-test.js
@@ -0,0 +1,15 @@
+const i2c = require('i2c-bus');
+const Oled = require('oled-i2c-bus');
+const font = require('oled-font-5x7');
+
+const i2cBus = i2c.openSync(1);
+
+const oled = new Oled(i2cBus, {
+  width: 128,
+  height: 64,
+  address: 0x3C
+});
+
+oled.clearDisplay();
+oled.setCursor(0, 0);
+oled.writeString(font, 1, 'HELLO PI', 1, true);
