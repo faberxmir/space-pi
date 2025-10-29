0c356933f6c73c7a1ab28efb9a61ff9d41b16c59
superpadde <padda@paddeby.com>
2025-10-29 11:57:01 +0100

    adding on and off controls for the leds to the homepage

--- public/js/buttons.js
+++ public/js/buttons.js
@@ -3,6 +3,9 @@ console.log('buttons.js loaded');
 const oledSendButton = document.querySelector('#oled-send');
 const oledInputField = document.querySelector('#oled-controls input[type="text"]');
 
+const onButton = document.querySelector('#on');
+const offButton = document.querySelector('#off');
+
 oledSendButton.addEventListener('click', () => {
     const text = oledInputField.value || 'Go goofy!'; 
     fetch(`/setText?text=${encodeURIComponent(text)}&x=0&y=0`)
@@ -12,5 +15,33 @@ oledSendButton.addEventListener('click', () => {
     })
     .catch(error => {
         console.error('Error sending text to OLED:', error);
+    });
+});
+
+onButton.addEventListener('click', () => {
+    fetch('/on')
+    .then(response => {
+        if(response.ok){
+            console.info('All lights turned ON');
+        } else {
+            console.error('Failed to turn on lights');
+        }
+    })
+    .catch(error => {
+        console.error('Error turning on lights:', error);
+    });
+});
+
+offButton.addEventListener('click', () => {
+    fetch('/off')
+    .then(response => {
+        if(response.ok){
+            console.info('All lights turned OFF');
+        } else {
+            console.error('Failed to turn OFF lights');
+        }
+    })
+    .catch(error => {
+        console.error('Error turning OFF lights:', error);
     });
 });
