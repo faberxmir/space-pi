2946910766fbddb36900b8587092306902adee64
superpadde <padda@paddeby.com>
2025-11-03 20:45:16 +0100

    adding a script to turn on all lights

---
+++ test-scripts/TB-ALL-ON.js
@@ -0,0 +1,22 @@
+const gpio = require('array-gpio');
+
+const DIN   = gpio.out(11);  // TB pin 2
+const CLK   = gpio.out(13);  // TB pin 3
+const LATCH = gpio.out(15);  // TB pin 4
+
+function pulse(pin) {
+  pin.on();
+  pin.off();
+}
+
+function allOn() {
+  // shift in 16 bits of "1"
+  for (let i = 0; i < 16; i++) {
+    DIN.on();
+    pulse(CLK);
+  }
+  pulse(LATCH);
+}
+
+allOn();
+console.log("All LEDs should now light");
