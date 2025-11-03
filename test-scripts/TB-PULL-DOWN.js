a3720d5ac8f05a7e4d95b179d55f486a91b87ece
superpadde <padda@paddeby.com>
2025-11-03 19:56:16 +0100

    adding more verbose outputs to make TB-PULL-DOWN easier to follow

--- test-scripts/TB-PULL-DOWN.js
+++ test-scripts/TB-PULL-DOWN.js
@@ -13,11 +13,14 @@ const sleep = ms => new Promise(r=>setTimeout(r, ms));
 
 runTest();
 async function runTest() {
+    console.log("Starting DIN HIGH clock test...");
     await sleep(5000);
     for(let i=0; i < 5; i++) {
+        console.log(`ON pulse ${i+1}/5`);
         CLK.on(); 
         await sleep(1000);
         CLK.off(); 
+        console.log(`OFF pulse ${i+1}/5`);
         await sleep(1000);
     }
 }
