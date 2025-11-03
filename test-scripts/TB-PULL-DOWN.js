2298283d0d0d374f75fcbed6245338da5fc26655
superpadde <padda@paddeby.com>
2025-11-03 20:10:56 +0100

    including tests for all control pins

--- test-scripts/TB-PULL-DOWN.js
+++ test-scripts/TB-PULL-DOWN.js
@@ -12,15 +12,36 @@ const sleep = ms => new Promise(r=>setTimeout(r, ms));
 
 
 runTest();
+
 async function runTest() {
-    console.log("Starting DIN HIGH clock test...");
+    console.log("Starting CLK test... measure pin 3...");
     await sleep(5000);
     for(let i=0; i < 5; i++) {
         console.log(`ON pulse ${i+1}/5`);
         CLK.on(); 
-        await sleep(1000);
+        await sleep(2000);
         CLK.off(); 
         console.log(`OFF pulse ${i+1}/5`);
-        await sleep(1000);
+        await sleep(2000);
+    }
+    console.log("Starting LATCH test... measure pin 4...");
+    await sleep(5000);
+    for(let i=0; i < 5; i++) {
+        console.log(`ON pulse ${i+1}/5`);
+        LATCH.on(); 
+        await sleep(2000);
+        LATCH.off(); 
+        console.log(`OFF pulse ${i+1}/5`);
+        await sleep(2000);
+    }
+    console.log("Starting DIN test... measure pin 2...");
+    await sleep(5000);
+    for(let i=0; i < 5; i++) {
+        console.log(`ON pulse ${i+1}/5`);
+        DIN.on(); 
+        await sleep(2000);
+        DIN.off(); 
+        console.log(`OFF pulse ${i+1}/5`);
+        await sleep(2000);
     }
 }
