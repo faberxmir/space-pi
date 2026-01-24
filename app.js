be2aeed525041409aba5081cacc71d4fd4db5441
superpadde <padda@paddeby.com>
2026-01-24 13:27:59 +0100

    fixing logical error in setup routine

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ require('dotenv').config();
 const express= require('express');
 const app = express();
 const defaultRoutes = require('./routers/default_routes');
-require('./handlers/pi_setup');
+const pi_setup = require('./handlers/pi_setup');
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.use('/', defaultRoutes);
+pi_setup(app);
 
 
 const PORT = process.env.PORT;
--- handlers/pi_setup.js
+++ handlers/pi_setup.js
@@ -2,10 +2,14 @@
 const os = require('os');
 const isRaspberryPi = os.platform() === 'linux' && os.arch() === 'arm64';
 
-if(isRaspberryPi){
-    const ledRoutes = require('../routers/led_routes');
-    app.use(ledRoutes);
-    console.info("Raspberry Pi detected. Initializing Pi-specific handlers.");
-} else {
-    console.warn("Not running on a Raspberry Pi. Startup limited to non-Pi functionality.");
+
+
+module.exports=(app) => {
+    if(isRaspberryPi){
+        const ledRoutes = require('../routers/led_routes');
+        app.use(ledRoutes);
+        console.info("Raspberry Pi detected. Initializing Pi-specific handlers.");
+    } else {
+        console.warn("Not running on a Raspberry Pi. Startup limited to non-Pi functionality.");
+    }
 }
