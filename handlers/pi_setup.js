1aa999adb3dd91804db7391e4c8c4340ed3e2e8c
superpadde <padda@paddeby.com>
2026-01-24 13:19:25 +0100

    adding some output for debug on pie

--- handlers/pi_setup.js
+++ handlers/pi_setup.js
@@ -1,10 +1,12 @@
 // Check if running on a Raspberry Pi
 const os = require('os');
 const isRaspberryPi = os.platform() === 'linux' && os.arch() === 'arm';
+console.log(os.platform(), os.arch());
 
 if(isRaspberryPi){
     const ledRoutes = require('./routers/led_routes');
     app.use(ledRoutes);
+    console.info("Raspberry Pi detected. Initializing Pi-specific handlers.");
 } else {
     console.warn("Not running on a Raspberry Pi. Startup limited to non-Pi functionality.");
 }
