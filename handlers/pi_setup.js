8bde9e87e9f7c00c8ba6169dfa97f46f7687d65f
superpadde <padda@paddeby.com>
2026-01-24 15:00:49 +0100

    adding led routes to root /led

--- handlers/pi_setup.js
+++ handlers/pi_setup.js
@@ -7,7 +7,7 @@ const isRaspberryPi = os.platform() === 'linux' && os.arch() === 'arm64';
 module.exports=(app) => {
     if(isRaspberryPi){
         const ledRoutes = require('../routers/led_routes');
-        app.use(ledRoutes);
+        app.use('/led', ledRoutes);
         console.info("Raspberry Pi detected. Initializing Pi-specific handlers.");
     } else {
         console.warn("Not running on a Raspberry Pi. Startup limited to non-Pi functionality.");
