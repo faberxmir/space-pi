c938c70a8532ce240f86d38402f8ef701b5a65d1
superpadde <padda@paddeby.com>
2026-01-31 18:03:14 +0100

    adding .env config from src/main.js pointing to ../.env

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 // Entry point of the Supertoad space war application
+require('dotenv').config({path: require('path').resolve(__dirname, '../.env')});
 const {bootstrap} = require('./bootstrap');
 
 bootstrap({logger: console});
