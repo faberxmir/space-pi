857864f51adad2591e1658feec4901fd79ae6a76
superpadde <padda@paddeby.com>
2026-02-12 20:00:12 +0100

    adding /tests/oled_min_test.js to isolate oled functionality

---
+++ tests/oled_min_test.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+const {createOledService} = require("../src/services/oled/index");
+
+(async () => {
+    const oled = createOledService({
+        i2cBusNumber: 1,
+        address: 0x3c,
+        logger: console,
+        height: 64,
+        width: 128,
+    });
+
+    await oled.init();
+
+
+    oled.phase("TEST_PHASE");
+    oled.render(["Hello, OLED!", "Line 2", "Line 3"]);
+
+    setTimeout(() => {
+        oled.shutdown();
+        console.log("OLED test completed");
+    }, 5000);
+})().catch(e => {
+    console.error("OLED test failed:", e);
+    process.exit(1);
+});
