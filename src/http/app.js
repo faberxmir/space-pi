fdf6006d20f71eeac35c2add4129b93dc012c3f7
superpadde <padda@paddeby.com>
2026-02-23 20:31:29 +0100

    fixing a wrong call to createapp in routesup

--- src/http/app.js
+++ src/http/app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const {createBuzzerRoutes} = require('./routes/buzzer_routes');
 
 
-
-
-module.exports = function createApp(context) {
+function createApp(context) {
     const app = express();
     app.use(express.json());
     app.use('/api/buzzer', createBuzzerRoutes({buzzerService: context.buzzerService, logger: context.logger}));
+    
     return app;
 }
+
+module.exports = {createApp };
